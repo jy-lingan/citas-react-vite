5eaf8d723ab27142ab1c1643d067274ec8fb44c6
Jimmy Lingan <jimmy.lingan@outlook.com>
2022-06-19 05:41:22 -0500

    Update header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <>
       <h1 className='font-black text-5xl text-center md:w-2/3 mx-auto'>
-        Seguimiento Pacientes {' '}
+        Seguimiento Pacientes {' '} <span className='text-red-500'>Con Github</span>
         <span className='text-indigo-600'>Veterinaria</span>
       </h1>
     </>
